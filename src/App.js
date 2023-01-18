2885cb2298075f6ec7908be26b2bbf5a18c65fdb
Siddhesh <siddhehsongire@gmail.com>
2023-01-18 17:50:13 +0530

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Exprience from './pages/Experience'
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Routes>
+          <Route path='/' element = {<Home/>} />
+          <Route path='/projects' element = {<Projects/>} />
+          <Route path='/experience' element = {<Exprience/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
