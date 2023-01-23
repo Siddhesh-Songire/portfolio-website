43a8b28d7514817565daf093b029357bb8ccbcbf
Siddhesh <siddhehsongire@gmail.com>
2023-01-23 23:04:21 +0530

    Added Project page

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,24 @@
+import img1 from "../assets/img1.png";
+
+export const ProjectList = [
+    {
+        name: "Social Media App",
+        image: img1,
+    },
+    {
+        name: "Todo MERN APP",
+        image: img1,
+    },
+    {
+        name: "A* Path Finding Visualizer",
+        image: img1,
+    },
+    {
+        name: "NewsApp",
+        image: img1,
+    },
+    {
+        name: "Textutils",
+        image: img1,
+    }
+];
